a162f3eab4e34815cba3cbf0ec64101634818780
deklan-w <deklan.white@allsaintshigh.org>
2025-08-29 11:07:11 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args)
+    {
+//define 1 variable of each of the
+//following data types
+//byte short int long
+//float double
+//char boolean String
+//integer variables
+                byte byteOne = 120;
+                short shortOne =-32120;
+                int intOne =100000;
+                long longOne =65535L;
+                char charOne ='A';
+                float floatOne =10010.6F;
+                double doubleOne =900.321768;
+                boolean booleanOne =true;
+                String stringOne ="Hello universe";
+                System.out.println("//////////////////////////////////////////");
+                System.out.println("*\t\tDeklan White 08/29/25\t\t\t*");
+                System.out.println("*\t\t\t\t\t\t\t\t\t\t*");
+                System.out.println("*\t\t\tinteger types\t\t\t\t*");
+                System.out.println("*\t\t\t\t\t\t\t\t\t\t*");
+                System.out.println("*\t\t8 bit - byteOne = "+byteOne+"\t\t\t*");
+                System.out.println("*\t\t16 bit - shortOne = "+shortOne+"\t\t*");
+                System.out.println("*\t\t32 bit - intOne = "+intOne+"\t\t*");
+                System.out.println("*\t\t64 bit - longOne = "+longOne+"\t\t*");
+                System.out.println("*\t\t\t\t\t\t\t\t\t\t*");
+                System.out.println("*\t\t\treal types\t\t\t\t\t*");
+                System.out.println("*\t\t\t\t\t\t\t\t\t\t*");
+                System.out.println("*\t\t32 bit - floatOne = "+floatOne+"\t\t*");
+                System.out.println("*\t\t64 bit - doubleOne = "+doubleOne+"\t*");
+                System.out.println("*\t\t\t\t\t\t\t\t\t\t*");
+                System.out.println("*\t\t\tother integer types\t\t\t*");
+                System.out.println("*\t\t\t\t\t\t\t\t\t\t*");
+                System.out.println("*\t\tbooleanOne = "+booleanOne+"\t\t\t\t*");
+                System.out.println("*\t\tstringOne = "+stringOne+"\t\t*");
+                System.out.println("*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*");
+
+
+            }
+        }
+
+
